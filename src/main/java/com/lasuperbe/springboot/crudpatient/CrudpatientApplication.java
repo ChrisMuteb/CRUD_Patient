c3bed125e1a53d3986e8b3394a53177977905acb
ChrisMuteb <ibond.chris@gmail.com>
2023-08-04 11:17:45 +0200

    Initial commit

---
+++ src/main/java/com/lasuperbe/springboot/crudpatient/CrudpatientApplication.java
@@ -0,0 +1,13 @@
+package com.lasuperbe.springboot.crudpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lasuperbe/springboot/crudpatient/CrudpatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasuperbe.springboot.crudpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
